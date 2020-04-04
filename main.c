4eac6dcd6055dc377e89da688c924332285a7483
OmarEmaraDev <mail@OmarEmara.dev>
2020-04-04 22:51:54 +0200

    Implement monte carlo simulations.

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <limits.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define NK_PRIVATE
 #define NK_INCLUDE_FIXED_TYPES
@@ -12,8 +13,66 @@
 #include "nuklear_xlib.h"
 
 #define MAX_SIZE 32
+#define DEFAULT_FREQUENCY 10
+#define DEFAULT_NUMBER_OF_SAMPLES 5
 
-int main(void) {
+void getProbabilities(float probabilities[MAX_SIZE], int frequencies[MAX_SIZE],
+                      int sampleSize) {
+  int total = 0;
+  for (int i = 0; i < sampleSize; i++) {
+    total += frequencies[i];
+  }
+  for (int i = 0; i < sampleSize; i++) {
+    probabilities[i] = (float)frequencies[i] / total;
+  }
+}
+
+int findRange(float cumulativeProbabilities[MAX_SIZE], int sampleSize,
+              double target) {
+  for (int i = 0; i < sampleSize - 1; i++) {
+    if (target > cumulativeProbabilities[i] &&
+        target <= cumulativeProbabilities[i + 1]) {
+      return i;
+    }
+  }
+  return sampleSize - 1;
+}
+
+void getCumulativeProbabilities(float cumulativeProbabilities[MAX_SIZE],
+                                float probabilities[MAX_SIZE], int sampleSize) {
+  float sum = 0.0f;
+  for (int i = 0; i < sampleSize; i++) {
+    sum += probabilities[i];
+    cumulativeProbabilities[i] = sum;
+  }
+}
+
+float getAverageDemand(float probabilities[MAX_SIZE], int demands[MAX_SIZE],
+                       int sampleSize, int numberOfSamples) {
+  float cumulativeProbabilities[MAX_SIZE];
+  getCumulativeProbabilities(cumulativeProbabilities, probabilities,
+                             sampleSize);
+
+  srand48(0);
+  int total = 0;
+  for (int i = 0; i < numberOfSamples; i++) {
+    double randomNumber = drand48();
+    total +=
+        demands[findRange(cumulativeProbabilities, sampleSize, randomNumber)];
+  }
+  return (float)total / numberOfSamples;
+}
+
+float getExpectedDemand(float probabilities[MAX_SIZE], int demands[MAX_SIZE],
+                        int sampleSize) {
+  float average = 0.0f;
+  for (int i = 0; i < sampleSize; i++) {
+    average += demands[i] * probabilities[i];
+  }
+  return average;
+}
+
+int main() {
   Display *display = XOpenDisplay(NULL);
   Window rootWindow = DefaultRootWindow(display);
   int screenNumber = XDefaultScreen(display);
@@ -50,6 +109,7 @@ int main(void) {
   int sampleSize = 0;
   int demands[MAX_SIZE];
   int frequencies[MAX_SIZE];
+  int numberOfSamples = DEFAULT_NUMBER_OF_SAMPLES;
 
   while (1) {
     XEvent event;
@@ -64,10 +124,9 @@ int main(void) {
     }
     nk_input_end(context);
 
-    if (nk_begin(context, "Inputs", nk_rect(50, 50, 400, 200),
+    if (nk_begin(context, "Inputs", nk_rect(50, 50, 400, 500),
                  NK_WINDOW_BORDER | NK_WINDOW_MOVABLE | NK_WINDOW_SCALABLE |
-                     NK_WINDOW_CLOSABLE | NK_WINDOW_MINIMIZABLE |
-                     NK_WINDOW_TITLE)) {
+                     NK_WINDOW_MINIMIZABLE | NK_WINDOW_TITLE)) {
       for (int i = 0; i < sampleSize; i++) {
         nk_layout_row_dynamic(context, 25, 2);
         nk_property_int(context, "#Demand:", 0, demands + i, INT_MAX, 1, 1.0f);
@@ -78,13 +137,26 @@ int main(void) {
       nk_layout_row_static(context, 30, 80, 1);
       if (nk_button_label(context, "Add Sample")) {
         demands[sampleSize] = sampleSize;
-        frequencies[sampleSize] = 10;
+        frequencies[sampleSize] = DEFAULT_FREQUENCY;
         sampleSize++;
       }
+
+      nk_layout_row_dynamic(context, 25, 2);
+      nk_value_int(context, "Number Of Samples",
+                   numberOfSamples * numberOfSamples);
+      nk_slider_int(context, 1, &numberOfSamples, 2.0E3, 10);
+
+      float probabilities[MAX_SIZE];
+      getProbabilities(probabilities, frequencies, sampleSize);
+
+      nk_layout_row_dynamic(context, 25, 1);
+      nk_value_float(context, "Average Demand",
+                     getAverageDemand(probabilities, demands, sampleSize,
+                                      numberOfSamples * numberOfSamples));
+      nk_value_float(context, "Expected Demand",
+                     getExpectedDemand(probabilities, demands, sampleSize));
     }
     nk_end(context);
-    if (nk_window_is_hidden(context, "Inputs"))
-      break;
 
     XClearWindow(display, window);
     nk_xlib_render(window, nk_rgb(30, 30, 30));
